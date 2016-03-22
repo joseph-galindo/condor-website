ef8b10f63cd5847b1f33f6d3f9909c20616fb7e7
Joseph Galindo <jgalindo@cdmprinceton.com>
2016-03-22 19:14:07 -0400

    some updates

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,30 @@ $(document).ready(function() {
 
 	console.log('document ready from app.js');
 
+	//function declarations
+
+	function checkNav() {
+	    var nav = $('.main-nav')
+	    var anchor = nav.find('a');
+	    var current = window.location.pathname.split('/')[1];
+	    // current = current.replace('.html','');
+
+	    for (var i = 0; i < anchor.length; i++) {
+
+	    	//this split is messy...doublecheck this works as intended in prod environment
+
+	        if((anchor[i].href).split('/')[3] == current) {
+	            anchor[i].className = "active";
+	        }
+
+	        if(current == '') {
+	        	anchor[0].className = "active";
+	        }
+	    }
+	}
+
+	//function executions
+
+	checkNav();
+
 });
